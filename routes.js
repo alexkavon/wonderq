abc440adb84c1b01af8038a83e70d1e8257be0ef
Alexander Kavon <hawk@alexkavon.com>
2018-02-07 17:16:02 -0500

    add messages, remove messages, needs work

--- routes.js
+++ routes.js
@@ -1,17 +1,44 @@
 const db = require('./db')
+const schema = require('./message')
 
 module.exports = function(app) {
 
     app.get('/messages', function(req, res) {
         //Send messages to client and add timeout for expiration (requeue)
-        db.get('*', function(err, result) {
-            res.json(result);
+
+        //Create result array
+        result = []
+
+        //get all keys from redis
+        db.keys('*', function(err, keys) {
+            //get all values
+            db.mget(keys, (err, r) => {
+                //TODO: update as timeout
+                //perhaps secondary index to track timed out keys
+                for (var v of r) {
+                    result.push(JSON.parse(v));
+                }
+                res.json(result)
+            });
         });
     });
 
     app.post('/messages', function(req, res) {
         //Add a new message and respond with msgID
-        db.set('mykey', 'This is my key');
-        res.json(true)
+        const valid = schema.validate({
+            topic: 'data',
+            payload: {
+                message: 'My message',
+                valid: true
+            },
+            ip: req.ip,
+            timeout: false
+        }, schema.schema).then((value) => {
+            db.set(value.id, JSON.stringify(value));
+            res.json({status: 'success', id: value.id});
+        }).catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        })
     });
 }
