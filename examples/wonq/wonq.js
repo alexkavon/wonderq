c90095f589fb94466e1799733cd71e492b41ef0a
Alexander Kavon <hawk@alexkavon.com>
2018-02-08 22:45:42 -0500

    added command example wonq

---
+++ examples/wonq/wonq.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+const program = require('commander');
+const wonderq = require('../../lib/wonderq');
+
+program.version('1.0.0')
+
+program.command('list [queues...]')
+    .option('-f, --flush', 'flush the queue(s)')
+    .action(listFunc);
+
+program.command('flush [queues...]')
+    .action(flushFunc);
+
+program.command('messages <queue> <n>')
+    .action(messagesFunc);
+
+function listFunc(queues, cmd) {
+    console.log('Currently active queues...');
+    //Get queue counts
+    wonderq.keyCount(queues, (err, lens) =>{
+        if (err !== undefined) {
+            console.error(err);
+            process.exit(1);
+        }
+
+        for (let i = 0; i < queues.length; i++) {
+            console.log('    Queue: ' + queues[i] + '...' + lens[i] + ' keys');
+        }
+
+        if (cmd.flush) {
+            flushFunc(queues)
+        } else {
+            process.exit(1);
+        }
+    })
+}
+
+function messagesFunc(queue, n, cmd) {
+    n = n || 10;
+
+    wonderq.tail(queue, n, (err, msgs) => {
+        if (err !== undefined) {
+            console.error(err);
+            process.exit(1);
+        }
+
+        if (msgs.length == 0) {
+            console.log('Queue: ' + queue + ' is empty.');
+        } else {
+            console.log(...msgs);
+        }
+        process.exit(1);
+    });
+}
+
+function flushFunc(queues, cmd) {
+    wonderq.flush(queues, (err) => {
+        if (err !== undefined) {
+            console.error(err);
+            process.exit(1);
+        }
+
+        console.log('Successfully flushed Queues:', ...queues);
+        process.exit(1);
+    })
+}
+
+program.parse(process.argv);
